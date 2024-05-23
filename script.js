9d1ecfc54541a09dbfdf8237258bab2c20c051c4
elijahmekhai2 <108166341+elijahmekhai2@users.noreply.github.com>
2024-05-23 11:31:09 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function startTime( ) {
+function updateTime() {
     const today = new Date ( );
     let h = today.getHours ( );
     let m = today.getMinutes ( );
     let s = today.getSeconds ( );
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    document.getElementById('timeDisplay').innerHTML = h + ":" + m + ":" + s;
         setTimeout(startTime, 1000);
 }
 
